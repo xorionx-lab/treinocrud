dccda776a6352934300d4d4a59f4d420ff8fc695
unknown <dennis.grupoaffectio@gmail.com>
2024-04-30 16:24:33 -0300

    atual

--- tela.py
+++ tela.py
@@ -1,1 +1,21 @@
 import streamlit as st
+
+st.title("Tela do banco de dados :alien:")
+st.header("Sistema de produtos")
+st.markdown("##Cadastro produtos")
+
+st.write("Esse programa foi desenvolvido como teste")
+
+
+
+nome = st.text_input("Nome do produto", placeholder="max 50 caracteres")
+preco = float(st.number_input("Digite o valor do produto: "))
+imagem = st.text_input("imagem do produto", placeholder="url da imagem produto de até 100 caract")
+codigo = st.text_input("Código do produto:", placeholder="codigo do produto")
+
+btncadastro = st.button("Cadastrar produto")
+
+if btncadastro:
+    cadastrar(nome, preco, codigo, imagem)
+    st.write("Cadastro feito com sucesso")
+

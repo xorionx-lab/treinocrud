4a0b18fda0d6106e24842c3af57a502dd441bc43
unknown <dennis.grupoaffectio@gmail.com>
2024-05-01 10:13:09 -0300

    atualização da tela

--- tela.py
+++ tela.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from funcao import cadastrar
 from funcao import atualizarpeco
+from funcao import deletar
 
 st.title("Tela do banco de dados :alien:")
 st.header("Sistema de produtos")
-st.markdown("##Cadastro produtos")
+st.markdown("**Cadastro produtos**")
 
 st.write("Esse programa foi desenvolvido como teste")
-
-
 
 nome = st.text_input("Nome do produto", placeholder="max 50 caracteres")
 preco = float(st.number_input("Digite o valor do produto: "))
@@ -22,14 +21,26 @@ if btncadastro:
     st.write("Cadastro feito com sucesso")
 
 st.markdown("----------------------------------------")
-st.write("Atualiação do valor")
+st.markdown("**Atualiação do valor**")
 novo_valor = float(st.number_input("Digite o novo valor do produto: "))
-novo = st.text_input("Código do produto", placeholder="codigo do produto")
+id = st.text_input("Código do produto", placeholder="codigo do produto")
 
 btnatualizarpreco = st.button("Atualizar preço")
 
 if btnatualizarpreco:
-    atualizarpeco(novo_valor, codigo)
+    atualizarpeco(id, novo_valor)
     st.write("Preço atualizado")
+
+st.markdown("---------------------------------------")
+
+st.markdown('**Deletar produto**')
+
+id = st.text_input("Código do produto a ser deletado", placeholder="código do produto")
+btndeletar = st.button("produto deletado")
+
+if btndeletar:
+    deletar(id)
+    st.write("Deletar produto")
+
 
 

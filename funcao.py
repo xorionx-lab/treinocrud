482d88e674ded9b0ee47da9956f66d6d226b6e09
xorionx-lab <dennis.grupoaffectio@gmail.com>
2024-05-11 16:18:44 -0300

    mensagem

--- funcao.py
+++ funcao.py
@@ -33,7 +33,7 @@ def atualizarpeco(id: str, novo_valor: float):
 #Função listartodos
 
 def selecionarTodosProdutos():
-    conexao, cursor = conectarBd()
+    conexao, cursor = conectarbd()
     comando_sql = f'select id, nome, preco from produtos'
     cursor.execute(comando_sql)
     resultado_consulta = cursor.fetchall()
